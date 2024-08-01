1f17bedd2b8901fe4afea1ccb7949a88871ae03d
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2024-08-01 18:44:17 +0300

    hm/desktop/music.sh: remove useless echo and add double quote

--- home-manager/desktop/scripts/music.sh
+++ home-manager/desktop/scripts/music.sh
@@ -2,12 +2,12 @@
 
 players="spotify|rhythmbox"
 pctl=$(playerctl -a metadata)
-m=$(echo $pctl | grep -E $players)
+m=$(echo "$pctl" | grep -E "$players")
 if [ -z "$u" ]; then
     artist=$(playerctl -a metadata | grep -E $players | grep xesam:artist | cut -d " " -f 3-)
     song=$(playerctl -a metadata | grep -E $players | grep xesam:title | cut -d " " -f 3-)
     #this might seem redundant, but it actually removes extra whitespace from strings.
-    echo $(echo "$artist - $song")
+    echo "$artist - $song"
 else
     echo "No Player Found"
 fi
