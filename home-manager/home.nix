27f2ef19d7beae5eb968b42b798a1d0fc988cd35
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2024-08-01 19:26:02 +0300

    hm/home: remove unneeded comments

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -8,10 +8,6 @@
 }:
 {
   imports = [
-    # If you want to use home-manager modules from other flakes (such as nix-colors):
-    # inputs.nix-colors.homeManagerModule
-
-    #inputs.hyprland.homeManagerModules.default
     ./hyprland/waybar.nix
     ./hyprland/hyprland.nix
     ./desktop/stylix.nix
@@ -19,35 +15,18 @@
   ];
 
   nixpkgs = {
-    # You can add overlays here
-    overlays = [
-      # If you want to use overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
-    ];
-    # Configure your nixpkgs instance
+    overlays = [ ];
     config = {
-      # Disable if you don't want unfree packages
       allowUnfree = true;
-      # Workaround for https://github.com/nix-community/home-manager/issues/2942
       allowUnfreePredicate = _: true;
     };
   };
 
-  # TODO: Set your username
   home = {
     username = "hushh";
     homeDirectory = "/home/hushh";
   };
 
-  # Add stuff for your user as you see fit:
-  #programs.neovim.enable = true;
   home.packages = with pkgs; [
     #base apps    
     firefox
@@ -111,7 +90,6 @@
     };
   };
 
-  # Enable home-manager and git
   programs = {
     home-manager.enable = true;
     git = {
