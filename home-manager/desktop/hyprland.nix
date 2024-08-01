ecd4f16a96018d7a6f7ff31f55e14de50342b53c
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2024-08-01 19:10:37 +0300

    hm/desktop/hyprland: use nix to generate {work,moveto}space dynamically

--- home-manager/desktop/hyprland.nix
+++ home-manager/desktop/hyprland.nix
@@ -147,72 +147,69 @@ _: {
       "$terminal" = "konsole";
       "$fileManager" = "thunar";
 
-      bind = [
-        "$mainMod, Space, exec, rofi -show drun"
-        "$Control_L&Alt_L, T, exec, $terminal"
-        "$Alt_L, 1, killactive,"
-        "$mainMod, M, exit,"
-        "$mainMod, E, exec, $fileManager"
-        "$mainMod, S, togglefloating,"
-        "$mainMod, P, pseudo," # dwindle
-        "$mainMod, J, togglesplit," # dwindle
+      bind =
+        [
+          "$mainMod, Space, exec, rofi -show drun"
+          "$Control_L&Alt_L, T, exec, $terminal"
+          "$Alt_L, 1, killactive,"
+          "$mainMod, M, exit,"
+          "$mainMod, E, exec, $fileManager"
+          "$mainMod, S, togglefloating,"
+          "$mainMod, P, pseudo," # dwindle
+          "$mainMod, J, togglesplit," # dwindle
 
-        # Switch workspaces with mainMod + [0-9]
-        "$mainMod, 1, workspace, 1"
-        "$mainMod, 2, workspace, 2"
-        "$mainMod, 3, workspace, 3"
-        "$mainMod, 4, workspace, 4"
-        "$mainMod, 5, workspace, 5"
-        "$mainMod, 6, workspace, 6"
-        "$mainMod, 7, workspace, 7"
-        "$mainMod, 8, workspace, 8"
-        "$mainMod, 9, workspace, 9"
-        "$mainMod, 0, workspace, 10"
+          # Move focus with mainMod + arrow keys
+          "$mainMod, left, movefocus, l"
+          "$mainMod, right, movefocus, r"
+          "$mainMod, up, movefocus, u"
+          "$mainMod, down, movefocus, d"
 
-        # Move active window to a workspace with mainMod + SHIFT + [0-9]
-        "$mainMod SHIFT, 1, movetoworkspace, 1"
-        "$mainMod SHIFT, 2, movetoworkspace, 2"
-        "$mainMod SHIFT, 3, movetoworkspace, 3"
-        "$mainMod SHIFT, 4, movetoworkspace, 4"
-        "$mainMod SHIFT, 5, movetoworkspace, 5"
-        "$mainMod SHIFT, 6, movetoworkspace, 6"
-        "$mainMod SHIFT, 7, movetoworkspace, 7"
-        "$mainMod SHIFT, 8, movetoworkspace, 8"
-        "$mainMod SHIFT, 9, movetoworkspace, 9"
-        "$mainMod SHIFT, 0, movetoworkspace, 10"
+          "$mainMod SHIFT, 0, movetoworkspace, 10"
 
-        # Move focus with mainMod + arrow keys
-        "$mainMod, left, movefocus, l"
-        "$mainMod, right, movefocus, r"
-        "$mainMod, up, movefocus, u"
-        "$mainMod, down, movefocus, d"
+          # Scroll through existing workspaces with mainMod + scroll
+          "$mainMod, mouse_down, workspace, e+1"
+          "$mainMod, mouse_up, workspace, e-1"
 
-        "$mainMod SHIFT, 0, movetoworkspace, 10"
+          # hyprlock keybind
+          "Control_L&Alt_L, L, exec, hyprlock"
 
-        # Scroll through existing workspaces with mainMod + scroll
-        "$mainMod, mouse_down, workspace, e+1"
-        "$mainMod, mouse_up, workspace, e-1"
+          # Audio Control
+          " ,XF86AudioRaiseVolume,exec,wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
+          " ,XF86AudioLowerVolume,exec,wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+          " ,XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+          " ,XF86AudioPlay, exec, playerctl play-pause"
+          " ,XF86AudioPause, exec, playerctl play-pause"
+          " ,XF86AudioNext, exec, playerctl next"
+          " ,XF86AudioPrev, exec, playerctl previous"
 
-        # hyprlock keybind
-        "Control_L&Alt_L, L, exec, hyprlock"
+          # Screenshot bind
+          ", Print, exec, grim -g \"$(slurp)\" - | swappy -f -"
+          # Edit image currently in clipboard
+          "Control_L, Print, exec, wl-paste | swappy -f -"
 
-        # Audio Control
-        " ,XF86AudioRaiseVolume,exec,wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
-        " ,XF86AudioLowerVolume,exec,wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
-        " ,XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
-        " ,XF86AudioPlay, exec, playerctl play-pause"
-        " ,XF86AudioPause, exec, playerctl play-pause"
-        " ,XF86AudioNext, exec, playerctl next"
-        " ,XF86AudioPrev, exec, playerctl previous"
+          "$mainMod, F, fullscreen"
 
-        # Screenshot bind
-        ", Print, exec, grim -g \"$(slurp)\" - | swappy -f -"
-        # Edit image currently in clipboard
-        "Control_L, Print, exec, wl-paste | swappy -f -"
-
-        "$mainMod, F, fullscreen"
-
-      ];
+        ]
+        ++ (
+          # workspaces
+          # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
+          builtins.concatLists (
+            builtins.genList (
+              x:
+              let
+                ws =
+                  let
+                    c = (x + 1) / 10;
+                  in
+                  builtins.toString (x + 1 - (c * 10));
+              in
+              [
+                "$mainMod, ${ws}, workspace, ${toString (x + 1)}"
+                "$mainMod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
+              ]
+            ) 10
+          )
+        );
 
       bindm = [
         "$mainMod, mouse:272, movewindow"
