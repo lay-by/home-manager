f5fec6c697e7d52bd2c6af422e465087a63fd255
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2024-08-01 18:22:40 +0300

    hm/desktop/scripts{music,nvidia,weather}.sh: format

--- home-manager/desktop/scripts/music.sh
+++ home-manager/desktop/scripts/music.sh
@@ -4,10 +4,10 @@ players="spotify|rhythmbox"
 pctl=$(playerctl -a metadata)
 m=$(echo $pctl | grep -E $players)
 if [ -z "$u" ]; then
-	artist=$(playerctl -a metadata | grep -E $players | grep xesam:artist | cut -d " " -f 3-  )
-	song=$(playerctl -a metadata | grep -E $players | grep xesam:title |  cut -d " " -f 3- )
-	#this might seem redundant, but it actually removes extra whitespace from strings.
-	echo $(echo "$artist - $song")
+    artist=$(playerctl -a metadata | grep -E $players | grep xesam:artist | cut -d " " -f 3-)
+    song=$(playerctl -a metadata | grep -E $players | grep xesam:title | cut -d " " -f 3-)
+    #this might seem redundant, but it actually removes extra whitespace from strings.
+    echo $(echo "$artist - $song")
 else
-	echo "No Player Found"
+    echo "No Player Found"
 fi
--- home-manager/desktop/scripts/nvidia.sh
+++ home-manager/desktop/scripts/nvidia.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 
-get_gpu(){
-TEMP=$(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader,nounits );
-GPU=$(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits );
+get_gpu() {
+    TEMP=$(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader,nounits)
+    GPU=$(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits)
 }
 get_gpu
 
 threshold=76
 
-if ((TEMP > threshold))
-then
-	echo "$GPU %{F#FE3120}$TEMP°C"
+if ((TEMP > threshold)); then
+    echo "$GPU %{F#FE3120}$TEMP°C"
 else
-	echo "$GPU% $TEMP°C"
+    echo "$GPU% $TEMP°C"
 fi
